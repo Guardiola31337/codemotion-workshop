07a856515229ec2926b5f9cfcb6674aa82f20997
pgs <pgs@tid.es>
2015-11-24 21:53:28 +0100

    create notes UI

---
+++ modules/app/src/main/java/com/codemotion2015/workshop/app/Notes.java
@@ -0,0 +1,72 @@
+package com.codemotion2015.workshop.app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class Notes extends AppCompatActivity {
+
+  private TextView title;
+  private TextView description;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.notes);
+
+    initToolbar();
+    initNote();
+    initFAB();
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_notes, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void initToolbar() {
+    final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+  }
+
+  private void initNote() {
+    title = (TextView) findViewById(R.id.title);
+    description = (TextView) findViewById(R.id.description);
+  }
+
+  private void initFAB() {
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+
+      }
+    });
+  }
+}
